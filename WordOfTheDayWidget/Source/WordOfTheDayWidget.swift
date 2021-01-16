8a1bd5a1a640816afacf2d43a92057e21dc864b0
Kyle <kdold02@gmail.com>
2021-01-16 16:49:58 +0000

    Added localizable.strings file

--- WordOfTheDayWidget/Source/WordOfTheDayWidget.swift
+++ WordOfTheDayWidget/Source/WordOfTheDayWidget.swift
@@ -11,13 +11,14 @@ import Intents
 
 @main
 struct WordOfTheDayWidget: Widget {
+    
     let kind: String = "WordOfTheDayWidget"
 
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: WordOfTheDayWidgetProvider()) { entry in
             WordOfTheDayWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName(LocalizedStringKey("widget.display_name"))
+        .description(LocalizedStringKey("widget.description"))
     }
 }
