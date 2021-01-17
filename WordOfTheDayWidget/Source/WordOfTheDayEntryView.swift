42fab8f636c42df14d8725c205a110a491801576
Kyle <kdold02@gmail.com>
2021-01-17 19:53:33 +0000

    Working on widget design

--- WordOfTheDayWidget/Source/WordOfTheDayEntryView.swift
+++ WordOfTheDayWidget/Source/WordOfTheDayEntryView.swift
@@ -12,6 +12,18 @@ struct WordOfTheDayWidgetEntryView: View {
     var entry: WordOfTheDayWidgetProvider.Entry
 
     var body: some View {
-        Text(entry.date, style: .time)
+        LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.vertical)
+            .overlay(
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Sample")
+                        .foregroundColor(Color.white)
+                        .bold()
+                    Text("This is the sample definition").italic()
+                        .foregroundColor(Color.white)
+                    Text("This is the sameple example usage")
+                        .foregroundColor(Color.white)
+                }
+            )
     }
 }
