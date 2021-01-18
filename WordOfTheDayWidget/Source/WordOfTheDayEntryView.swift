1cc54b145db76bc29e00ff2566fe901e259856c1
Kyle <kdold02@gmail.com>
2021-01-18 17:41:19 +0000

    Added custom fonts

--- WordOfTheDayWidget/Source/WordOfTheDayEntryView.swift
+++ WordOfTheDayWidget/Source/WordOfTheDayEntryView.swift
@@ -12,18 +12,14 @@ struct WordOfTheDayWidgetEntryView: View {
     var entry: WordOfTheDayWidgetProvider.Entry
 
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .top, endPoint: .bottom)
-            .edgesIgnoringSafeArea(.vertical)
-            .overlay(
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Sample")
-                        .foregroundColor(Color.white)
-                        .bold()
-                    Text("This is the sample definition").italic()
-                        .foregroundColor(Color.white)
-                    Text("This is the sameple example usage")
-                        .foregroundColor(Color.white)
-                }
-            )
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Sample")
+                .foregroundColor(Color.black)
+                .bold()
+            Text("This is the sample definition").italic()
+                .foregroundColor(Color.black)
+            Text("\"This is the sameple example usage\"")
+                .foregroundColor(Color.black)
+        }
     }
 }
