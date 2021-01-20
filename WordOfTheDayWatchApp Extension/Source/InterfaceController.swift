684bdc4060854779440f66e03cd880fbb036727c
Kyle <kdold02@gmail.com>
2021-01-20 17:14:54 +0000

    Added Apple Watch target

---
+++ WordOfTheDayWatchApp Extension/Source/InterfaceController.swift
@@ -0,0 +1,25 @@
+//
+//  InterfaceController.swift
+//  WordOfTheDayWatchApp Extension
+//
+//  Created by Kyle Dold on 20/01/2021.
+//
+
+import WatchKit
+import Foundation
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
