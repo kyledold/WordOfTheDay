beb33d78ac7fd426a10a6130ad3712a4d69068be
Kyle <kdold02@gmail.com>
2021-01-23 11:49:21 +0000

    Created separate project for NetworkKit and made gave it a WatchOS and iOS target

--- NetworkKit/Resources/NetworkKit.h
+++ NetworkKit/Resources/NetworkKit.h
@@ -2,17 +2,17 @@
 //  NetworkKit.h
 //  NetworkKit
 //
-//  Created by Kyle Dold on 16/01/2021.
+//  Created by Kyle Dold on 23/01/2021.
 //
 
 #import <Foundation/Foundation.h>
 
 //! Project version number for NetworkKit.
-FOUNDATION_EXPORT double NetworkKitVersionNumber;
+FOUNDATION_EXPORT double NetworkKit_VersionNumber;
 
 //! Project version string for NetworkKit.
-FOUNDATION_EXPORT const unsigned char NetworkKitVersionString[];
+FOUNDATION_EXPORT const unsigned char NetworkKit_VersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <NetworkKit/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkKit_iOS/PublicHeader.h>
 
 
